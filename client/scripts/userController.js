7b54d35047eb307d2a66b94c7b3fc606ac3c4d05
abijith-la <abijithla@gmail.com>
2023-05-22 05:20:06 +0530

    .

--- client/scripts/userController.js
+++ client/scripts/userController.js
@@ -50,40 +50,68 @@ app.controller('userController', function ($scope, $http) {
     var canvas = document.getElementById('myCanvas')
     var ctx = canvas.getContext('2d')
 
-    for (var i = 0; i < 400; i = i + 25) {
-        ctx.beginPath()
-        ctx.moveTo(0,i)
-        ctx.lineTo(400,i)
-        ctx.stroke()
-        ctx.closePath()
+    // for (var i = 0; i < 400; i = i + 25) {
+    //     ctx.beginPath()
+    //     ctx.moveTo(0, i)
+    //     ctx.lineTo(400, i)
+    //     ctx.stroke()
+    //     ctx.closePath()
+    // }
+    // for (var i = 0; i < 400; i = i + 25) {
+    //     ctx.beginPath()
+    //     ctx.moveTo(i, 0)
+    //     ctx.lineTo(i, 400)
+    //     ctx.stroke()
+    //     ctx.closePath()
+    // }
+
+    // ctx.beginPath()
+    // ctx.arc(200, 200, 100, 0, 7)
+    // ctx.stroke()
+    // ctx.closePath()
+
+    // ctx.beginPath()
+    // ctx.arc(200, 70, 50, 2.3, 7.1)
+    // ctx.stroke()
+    // ctx.closePath()
+
+    // ctx.beginPath()
+    // ctx.arc(250, 30, 20, 3.2, 8)
+    // ctx.stroke()
+    // ctx.closePath()
+
+    // ctx.beginPath()
+    // ctx.arc(150, 30, 20, 1.3, 6.3)
+    // ctx.stroke()
+    // ctx.closePath()
+    var isDrawing = false;
+
+    // Function to start drawing
+    function startDrawing(e) {
+        isDrawing = true;
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     }
-    for (var i = 0; i < 400; i = i + 25) {
-        ctx.beginPath()
-        ctx.moveTo(i,0)
-        ctx.lineTo(i,400)
-        ctx.stroke()
-        ctx.closePath()
+
+    // Function to continue drawing
+    function draw(e) {
+        if (isDrawing) {
+            ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+            ctx.stroke();
+        }
     }
 
-    ctx.beginPath()
-    ctx.arc(200, 200, 100, 0, 7)
-    ctx.stroke()
-    ctx.closePath()
+    // Function to stop drawing
+    function stopDrawing() {
+        isDrawing = false;
+        ctx.closePath();
+    }
 
-    ctx.beginPath()
-    ctx.arc(200, 70, 50, 2.3, 7.1)
-    ctx.stroke()
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.arc(250, 30, 20, 3.2, 8)
-    ctx.stroke()
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.arc(150, 30, 20, 1.3, 6.3)
-    ctx.stroke()
-    ctx.closePath()
+    // Event listeners for mouse events
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseleave', stopDrawing);
 
 
 })
