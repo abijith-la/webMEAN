818ccba96f300a7c05374553532941ecb27d819d
abijith-la <abijithla@gmail.com>
2023-05-22 05:12:06 +0530

    .

--- client/scripts/userController.js
+++ client/scripts/userController.js
@@ -47,63 +47,44 @@ app.controller('userController', function ($scope, $http) {
                 console.log(err)
             })
     }
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
+    var canvas = document.getElementById('myCanvas')
+    var ctx = canvas.getContext('2d')
 
-    // Draw the teddy bear body
-    ctx.beginPath();
-    ctx.arc(200, 200, 100, 0, 2 * Math.PI);
-    ctx.fillStyle = 'brown';
-    ctx.fill();
-    ctx.closePath();
+    for (var i = 0; i < 400; i = i + 25) {
+        ctx.beginPath()
+        ctx.moveTo(0,i)
+        ctx.lineTo(400,i)
+        ctx.stroke()
+        ctx.closePath()
+    }
+    for (var i = 0; i < 400; i = i + 25) {
+        ctx.beginPath()
+        ctx.moveTo(i,0)
+        ctx.lineTo(i,400)
+        ctx.stroke()
+        ctx.closePath()
+    }
 
-    // Draw the teddy bear head
-    ctx.beginPath();
-    ctx.arc(200, 120, 60, 0, 2 * Math.PI);
-    ctx.fillStyle = 'brown';
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.arc(200, 200, 100, 0, 7)
+    ctx.stroke()
+    ctx.closePath()
 
-    // Draw the teddy bear ears
-    ctx.beginPath();
-    ctx.moveTo(140, 100);
-    ctx.lineTo(160, 70);
-    ctx.lineTo(200, 120);
-    ctx.lineTo(240, 70);
-    ctx.lineTo(260, 100);
-    ctx.fillStyle = 'brown';
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.arc(200, 70, 50, 2.3, 7.1)
+    ctx.stroke()
+    ctx.closePath()
 
-    // Draw the teddy bear eyes
-    ctx.beginPath();
-    ctx.arc(180, 100, 10, 0, 2 * Math.PI);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.arc(250, 30, 20, 3.2, 8)
+    ctx.stroke()
+    ctx.closePath()
 
-    ctx.beginPath();
-    ctx.arc(220, 100, 10, 0, 2 * Math.PI);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.arc(150, 30, 20, 1.3, 6.3)
+    ctx.stroke()
+    ctx.closePath()
 
-    // Draw the teddy bear nose
-    ctx.beginPath();
-    ctx.moveTo(200, 115);
-    ctx.lineTo(195, 125);
-    ctx.lineTo(205, 125);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-    ctx.closePath();
-
-    // Draw the teddy bear mouth
-    ctx.beginPath();
-    ctx.arc(200, 135, 30, 0, Math.PI);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-    ctx.closePath();
 
 })
 
